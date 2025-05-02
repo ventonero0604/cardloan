3cfe8501ed92881e167292a801eadfb54e7788a4
akira <burningsunburst@yahoo.co.jp>
2025-05-02 16:25:09 +0900

    検索結果に注釈出す

--- app/result.php
+++ app/result.php
@@ -382,7 +382,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <?php require './components/categories.php' ?>
     </main>
 
-    <?php require './components/attention.php' ?>
+    <section class="Attention">
+      <?php
+      foreach ($filteredResults as $result) {
+        if ($result && isset($result['serviceName']) && isset($result['noteAll'])) {
+          echo '<p>';
+          echo '■' . htmlspecialchars((string)$result['serviceName'], ENT_QUOTES, 'UTF-8') . '<br>';
+          echo htmlspecialchars_decode((string)$result['noteAll'], ENT_QUOTES);
+          echo '</p>';
+        }
+      }
+      ?>
+      <p>
+        ＜当ページランキングについて＞<br>
+        当サイトで選定した『即日融資が可能なカードローン』を当ページからのアクセス数に基づき掲載しております。（当サイト調べ）<br><br>
+        ＜当サイトについて＞<br>
+        Yカードローンガイドでは、アフィリエイトプログラムを利用し、広告収益を得て運営しております。<br>
+        ※委託元：アコム、プロミス、アイフル、モビット、レイク、三井住友銀行、埼玉りそな銀行、北陽銀行<br><br>
+        ＜利用者数について＞<br>
+        調査目的：各カードローン及びキャッシングサービスのアクセス数により選ばれているサービスを紹介するため<br>
+        調査対象：国内からの当サイトアクセス<br>
+        集計期間：過去30日間 ※2021/12/23より定期的に集計<br>
+        調査方法：当サイト掲載サービスのホームページへのアクセス数を算出<br>
+        備考：同IPアドレス、同一企業（人物）と類推されるユーザーからのアクセスについては集計対象外<br>
+        調査主体者：mycardloan-guide.com→調査結果
+      </p>
+    </section>
     <?php require './components/footer.php' ?>
   </div>
 
